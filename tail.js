07193fee11ecee9cb07d589eeb1209d9c16b871d
Jill Regan <jillian.jnr@hotmail.com>
2023-11-13 19:22:59 -0500

    more changes

--- tail.js
+++ tail.js
@@ -8,7 +8,7 @@ const assertEqual = function(actual, expected) {
 const myArr = [1, 5, 6, 7]
 
 const tail = function(arr) {
-  let newArr = arr.slice(1, arr.length);
+  let newArr = arr.slice(1);
   return newArr;
 };
 
@@ -16,5 +16,6 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs");
-console.log(tail(myArr))
-console.log(myArr)
+
+const val = tail(myArr);
+assertEqual(val.length, myArr);
