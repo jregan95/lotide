b5a0fe13a4c66a457aa6a5a71bbb934ced17cbae
Jill Regan <jillian.jnr@hotmail.com>
2023-11-16 17:54:46 -0500

    finding key in an object using callback function

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  let test = Object.entries(object);
+  for (let i = 0; i < test.length; i++) {
+    if (callback(test[i][1])) {
+      return test[i][0];
+    }
+  }
+  
+}
+
+let test = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
+assertEqual(test, 'noma');
