66407c9532f842a4ab97267233648cd4d5e5dec8
Jill Regan <jillian.jnr@hotmail.com>
2023-11-15 19:54:56 -0500

    Added inspect to failed log

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -34,7 +34,7 @@ const assertObjectsEqual = function(actual, expected) {
   if (eqObjects(actual, expected)) {
     return console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} ===  ${inspect(expected)}`);
   }
-  return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  return console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
 
 };
 
