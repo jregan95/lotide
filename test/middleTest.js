c19e9b8b5eb9f5109b5e41d36476d7486433fc5a
Jill Regan <jillian.jnr@hotmail.com>
2023-11-19 22:19:08 -0500

    changed tests to use Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,19 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-const test = [1, 3, 5, 6];
 
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle(test), [3, 5]);
-assertArraysEqual(test, [1, 3, 5, 6]);
+describe("#middle", () => {
+
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,29 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
 
+describe("tail", () => {
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+  it("returns [2, 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
 
-const myArr = [1, 5, 6, 7];
-const val = tail(myArr);
-assertEqual(val.length , myArr.length - 1);
+  it("returns ['hi', 'bye'] for ['yes, 'hi, 'bye]", () => {
+    assert.deepEqual(tail(['yes', 'hi', 'bye']), ['hi', 'bye']);
+  });
 
-const secArr = [1];
-const secVal = tail(secArr);
-assertEqual(secVal.length, secArr. length - 1);
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
 
-const thirdArr = [];
-const thirdVal = tail(thirdArr);
-assertEqual(thirdVal.length, 0);
+  it("returns [1] for [0, 1]", () => {
+    assert.deepEqual(tail([0, 1]), [1]);
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+});
+
+
