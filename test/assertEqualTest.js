dc36f177730b36fbae296d30b044ea74472d672e
Jill Regan <jillian.jnr@hotmail.com>
2023-11-19 20:58:25 -0500

    Created test files for lotide functions

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, "1");
+assertEqual(1, 1);
+assertEqual("Hello World", "Hello World");
+assertEqual(15, 6);
